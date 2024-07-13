f689470a3b333480103c954761437fab0fe46adc
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-13 18:50:25 +0800

    fe - styling Google Auth Component

--- client/src/components/AuthButton/GAuthButton.tsx
+++ client/src/components/AuthButton/GAuthButton.tsx
@@ -1,7 +1,12 @@
-
+import { FaGoogle } from "react-icons/fa";
 
 export const GAuthButton = () => {
   return (
-    <button>GAuthButton</button>
-  )
-}
+    <button className="btn flex items-center bg-red-600 text-white w-full justify-center gap-3 hover:bg-red-400">
+      <span>
+        <FaGoogle />
+      </span>
+      <span className="text-lg">Continue to Google</span>
+    </button>
+  );
+};
