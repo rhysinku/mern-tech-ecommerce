81a4045a0e402448c3f30b22968d820ec687a2b7
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-14 00:24:42 +0800

    fe - swap style in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.scss";
 import "./styles/components.scss";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
