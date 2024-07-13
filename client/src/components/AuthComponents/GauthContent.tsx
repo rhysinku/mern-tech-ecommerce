be70775f39c76b33e56b15efe245f477127bd6e3
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-13 20:35:11 +0800

    fe - finishing styling the login page

--- client/src/components/AuthComponents/GauthContent.tsx
+++ client/src/components/AuthComponents/GauthContent.tsx
@@ -6,7 +6,7 @@ export const GauthContent = () => {
       <div>
         <GAuthButton />
       </div>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-3 items-center mt-6">
         <span className="w-full h-[1px]  bg-bodyColor-500 inline"></span>
         <span className="text-lg ">or</span>
         <span className="w-full h-[1px]  bg-bodyColor-500 inline"></span>
--- client/src/pages/AuthPages/AuthLogin.tsx
+++ client/src/pages/AuthPages/AuthLogin.tsx
@@ -5,25 +5,31 @@ export const AuthLogin = () => {
   return (
     <div className="block w-full">
       <div>
-        <h1 className="text-4xl">Sign In</h1>
+        <h1 className="text-6xl">Sign In</h1>
       </div>
       <form action="" className="flex flex-col gap-5 mt-6">
         <div>
           <GauthContent />
         </div>
-        <div>
+        <div className="flex flex-col gap-6">
           <div>
-            <label>Username or Email</label>
-            <input type="text" />
+            <label className="mb-4 block">Username or Email</label>
+            <input
+              className="w-full h-full border-bodyColor-600 border-[1px] rounded-sm py-3 px-2 "
+              type="text"
+            />
           </div>
           <div>
-            <label>Password</label>
-            <input type="password" />
+            <label className="mb-4 block">Password</label>
+            <input
+              className="w-full h-full border-bodyColor-600 border-[1px] rounded-sm py-3 px-2 "
+              type="password"
+            />
           </div>
           <button className="btn btn_primary w-full">Login</button>
         </div>
       </form>
-      <p className="italic ">
+      <p className="italic mt-5">
         Don’t have an account?{" "}
         <Link to="/signup" className="hover:underline">
           Sign Up
