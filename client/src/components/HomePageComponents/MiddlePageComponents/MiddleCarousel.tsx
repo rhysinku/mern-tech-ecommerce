aa89f23ca56b8eb5cbd692b7d72cfef7e18d4598
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-20 13:17:44 +0800

    fe- making json for new arrival data placeholder

--- client/src/components/HomePageComponents/MiddlePageComponents/MiddleCarousel.tsx
+++ client/src/components/HomePageComponents/MiddlePageComponents/MiddleCarousel.tsx
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import newArrivalJson from "../../../placeholderJson/newArrival.json";
+import "swiper/css";
+
 export const MiddleCarousel = () => {
-  return <div></div>;
+  return <div>
+    
+  </div>;
 };
