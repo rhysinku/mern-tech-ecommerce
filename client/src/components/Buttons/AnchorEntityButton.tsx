22359be8dac5b4fc73616a4ca6fcdc42f820b33d
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-20 18:48:51 +0800

    fe - create entity button

--- client/src/components/Buttons/AnchorEntityButton.tsx
+++ client/src/components/Buttons/AnchorEntityButton.tsx
@@ -1,3 +1,13 @@
-export const AnchorEntityButton = () => {
-  return <div>AnchorEntityButton</div>;
+import { Link } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
+export const AnchorEntityButton: React.FC = () => {
+  return (
+    <div>
+      <Link to="/">
+        <span>
+          <FaArrowRightLong />
+        </span>
+      </Link>
+    </div>
+  );
 };
