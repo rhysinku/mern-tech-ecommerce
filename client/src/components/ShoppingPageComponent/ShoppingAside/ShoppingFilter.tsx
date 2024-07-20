40c73c86edd5f498f9e527eca5ed208bb529dac0
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-20 23:09:39 +0800

    fe - formating category filter

--- client/src/components/ShoppingPageComponent/ShoppingAside/ShoppingFilter.tsx
+++ client/src/components/ShoppingPageComponent/ShoppingAside/ShoppingFilter.tsx
@@ -1,5 +1,21 @@
+const category = ["Computers", "Laptops", "Consoles", "Accessories"];
+
 function ShoppingFilter() {
-  return <div>ShoppingFilter</div>;
+  return (
+    <section>
+      <h3>Category</h3>
+      <div className="mt-2">
+        <form action="">
+          {category.map((item, index) => (
+            <div className="flex items-center gap-2" key={index}>
+              <input type="checkbox" />
+              <label>{item}</label>
+            </div>
+          ))}
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default ShoppingFilter;
