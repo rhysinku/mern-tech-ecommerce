1a4d35f0bfc00587aadc7e5adbba8f64aa84db20
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-21 14:50:47 +0800

    fe - display the query to the product section

--- client/src/pages/ShoppingPage/ShoppingPageHome.tsx
+++ client/src/pages/ShoppingPage/ShoppingPageHome.tsx
@@ -1,9 +1,15 @@
+import useQuery from "../../hooks/useQuery";
+
 export const ShoppingPageHome = () => {
+  const query = useQuery();
+  const category = query.get("category");
+
   return (
     <>
       <div className="w-full rounded-sm bg-slate-50 px-3 py-9">
         <div className="text-center text-8xl">
           <h1>Shopping Page Home</h1>
+          <p>{category || `No Category Selected`}</p>
         </div>
       </div>
     </>
