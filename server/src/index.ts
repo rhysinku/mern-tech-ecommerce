1d9b524b546369915f5c703a9f791c8b4ef57c32
rhysinku <rhysinvillahermosa16@gmail.com>
2024-07-29 18:16:03 +0800

    be - adding mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import dotenv from "dotenv"
+import mongoose from 'mongoose';
 
+dotenv.config();
 const app = express();
-const port = 6969; 
+const port = process.env.PORT || 6969; 
+
+
 
 app.get('/', (req, res)=>{
     res.status(200).send('Hello World!');
 })
 
-app.listen(port!, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+
+
+
+mongoose.connect(process.env.MONGO_APPLICATION_CODE!).then(()=>{
+    console.log("Connected to MongoDB");
+    // app.listen(port!, ()=>{
+    //      console.log(`Server is running on port http://localhost:${port}`);
+    // }) 
+}).catch(console.error)
